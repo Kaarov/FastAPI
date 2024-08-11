3c5c3ad9bd77c81cbbec09780560993ad2107768
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-11 12:24:10 +0600

    feat: Path Parameters

--- freeCodeCamp/Code With Tomi/myapi.py
+++ freeCodeCamp/Code With Tomi/myapi.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    }
+}
 
 
 @app.get("/")
 def index():
     return {"name": "First Name"}
+
+
+@app.get("/get-students/{student_id}")
+def get_student(student_id: int = Path(description="The ID of the student you want to view", gt=0, lt=3)):
+    return students[student_id]
