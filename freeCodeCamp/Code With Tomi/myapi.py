ca78e03a867f0255f857c6f9d0c849a744441a5b
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-11 12:32:46 +0600

    feat: Query Parameters

--- freeCodeCamp/Code With Tomi/myapi.py
+++ freeCodeCamp/Code With Tomi/myapi.py
@@ -1,7 +1,8 @@
+from typing import Optional
+
 from fastapi import FastAPI, Path
 
 app = FastAPI()
-
 
 students = {
     1: {
@@ -20,3 +21,11 @@ def index():
 @app.get("/get-students/{student_id}")
 def get_student(student_id: int = Path(description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student_by_name(*, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"msg": "Student not found"}
