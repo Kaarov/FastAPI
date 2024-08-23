5c07342604d1e8bb9c373cb46e2245497fb5544a
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-23 16:41:22 +0600

    feat: Базовая конфигурация

--- mahenzon/fastapi-example-base-app/main.py
+++ mahenzon/fastapi-example-base-app/main.py
@@ -1,7 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from core.config import settings
+
+from api import router as api_router
+
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", reload=True)
