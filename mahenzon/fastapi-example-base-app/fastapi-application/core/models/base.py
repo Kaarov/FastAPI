4bba1fc82e0dc136258b72c6a80f55ea506054b3
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-23 18:12:51 +0600

    feat: Базовая модель SQLAlchemy

---
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import declared_attr
+
+from utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
