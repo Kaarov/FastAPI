7882eb8c6e52a21925c3d3bf371afc4c56a957d0
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-23 19:52:58 +0600

    feat: SQLA id mixin

--- mahenzon/fastapi-example-base-app/fastapi-application/core/models/base.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/models/base.py
@@ -1,6 +1,5 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import declared_attr
 
 from core.config import settings
@@ -17,5 +16,3 @@ class Base(DeclarativeBase):
     @declared_attr
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
 
 
-class User(Base):
+class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     foo: Mapped[int]
     bar: Mapped[int]
