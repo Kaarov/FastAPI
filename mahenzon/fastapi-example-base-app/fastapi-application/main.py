5d2ff74219afc5dd43267b696a4b66854368e02e
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-23 20:34:50 +0600

    feat: FastAPI вернуть доступ к Swagger | Кастомная статика для документации (swagger, redoc)

--- mahenzon/fastapi-example-base-app/fastapi-application/main.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/main.py
@@ -1,32 +1,23 @@
-from contextlib import asynccontextmanager
-
 import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
 
 from core.config import settings
 
 from api import router as api_router
-from core.models import db_helper
+from create_fastapi_app import create_app
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    await db_helper.dispose()
-
-
-main_app = FastAPI(
-    default_response_class=ORJSONResponse,
-    lifespan=lifespan,
+main_app = create_app(
+    create_custom_static_urls=True,
 )
+
 main_app.include_router(
     api_router,
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
     )
