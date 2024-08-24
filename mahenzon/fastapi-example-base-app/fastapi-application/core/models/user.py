3718296454fdf9aeec3cb3f5f23aa42e5f824109
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-24 21:08:23 +0600

    feat: Модель User (пользователь) + id int pk

--- mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
@@ -1,14 +1,11 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
 
 from .base import Base
-from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.int_id_pk import IdIntPkMixin
 
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
-
-    __table_args__ = (UniqueConstraint("foo", "bar"),)
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
