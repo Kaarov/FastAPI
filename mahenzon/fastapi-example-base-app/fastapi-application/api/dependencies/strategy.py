4b8ebc7e3bee78fd1332ee6ae78f7633c9c5a53c
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-24 22:03:02 +0600

    feat: Стратегия для работы с токеном

--- mahenzon/fastapi-example-base-app/fastapi-application/api/dependencies/strategy.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/api/dependencies/strategy.py
@@ -2,23 +2,24 @@ from typing import TYPE_CHECKING, Annotated
 
 from fastapi import Depends
 from fastapi_users.authentication.strategy.db import (
-    AccessTokenDatabase,
     DatabaseStrategy,
 )
 
+from .access_tokens import get_access_token_db
 from core.config import settings
 
 if TYPE_CHECKING:
     from core.models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 
 def get_database_strategy(
     access_token_db: Annotated[
-        AccessTokenDatabase["AccessToken"],
+        "AccessTokenDatabase[AccessToken]",
         Depends(get_access_token_db),
     ],
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
-        access_token_db,
+        database=access_token_db,
         lifetime_seconds=settings.access_token.lifetime_seconds,
     )
