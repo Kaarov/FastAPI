cfc480b85443b5c9582ebd1cb3908f80b31bbce9
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-24 21:47:50 +0600

    feat: Миграция для access tokens

---
+++ mahenzon/fastapi-example-base-app/fastapi-application/alembic/versions/2024_08_24_2145-2894cdf4e8a3_create_access_tokens_table.py
@@ -0,0 +1,51 @@
+"""create access_tokens table
+
+Revision ID: 2894cdf4e8a3
+Revises: 89827556aec5
+Create Date: 2024-08-24 21:45:26.132770
+
+"""
+
+from typing import Sequence, Union
+
+import fastapi_users_db_sqlalchemy
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "2894cdf4e8a3"
+down_revision: Union[str, None] = "89827556aec5"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "access_tokens",
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            name=op.f("fk_access_tokens_user_id_users"),
+            ondelete="cascade",
+        ),
+        sa.PrimaryKeyConstraint("token", name=op.f("pk_access_tokens")),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_access_tokens_created_at"), table_name="access_tokens")
+    op.drop_table("access_tokens")
--- mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/models/user.py
@@ -1,11 +1,19 @@
-from fastapi_users.db import (
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
-
+from core.types.user_id import UserIdType
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
