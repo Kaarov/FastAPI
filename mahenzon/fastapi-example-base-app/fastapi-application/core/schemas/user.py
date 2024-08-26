02f4ad4d78e93cca739461c54863b20ea8fc9a0c
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-26 11:53:48 +0600

    fix: fixed UserRead Schema

--- mahenzon/fastapi-example-base-app/fastapi-application/core/schemas/user.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/core/schemas/user.py
@@ -3,7 +3,7 @@ from fastapi_users import schemas
 from core.types.user_id import UserIdType
 
 
-class UserRead(schemas.BaseModel[UserIdType]):
+class UserRead(schemas.BaseUser[UserIdType]):
     pass
 
 
