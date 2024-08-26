933a3de14d0b3ea8f7139fd1d70f29df48c0886f
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-26 11:36:15 +0600

    feat: Зависимость fastapi users для регистрации роутеров

--- mahenzon/fastapi-example-base-app/fastapi-application/api/dependencies/backend.py
+++ mahenzon/fastapi-example-base-app/fastapi-application/api/dependencies/backend.py
@@ -3,7 +3,7 @@ from fastapi_users.authentication import AuthenticationBackend
 from .strategy import get_database_strategy
 from core.authentication.transport import bearer_transport
 
-auth_backend = AuthenticationBackend(
+authentication_backend = AuthenticationBackend(
     name="access-tokens-db",
     transport=bearer_transport,
     get_strategy=get_database_strategy,
